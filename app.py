b9162a8da7e4e1c3c353f1135bd10c165b7b1ca2
MaximWuyts <32163906+MaximWuyts@users.noreply.github.com>
2019-01-14 21:34:56 +0100

    errors

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for, session, redirect, g
+from flask import Flask, render_template, request, redirect, url_for, session, redirect, g, flash
 import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
-conn = sqlite3.connect('products1.db', check_same_thread=False)
-cursor = conn.execute("SELECT * from products1")
+conn = sqlite3.connect('wuyts_maxim.db', check_same_thread=False)
+cursor = conn.execute("SELECT * from products")
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         session.pop("user", None)
         session["user"] = request.form['username']
         session["color"] = request.form["color"]
         return redirect(url_for("list"))
-   
-    return render_template("index.html", name = g.user)
-
+    try:
+        return render_template("index.html", name = g.user)
+    except Exception as e:
+        return (str(e))
 #show list if logged in
 @app.route('/list')
 def list():
     if g.user:
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
-        cur.execute("select * from products1")
+        cur.execute("select * from products")
    
         rows = cur.fetchall()
         if 'visits' in session:
@@ -31,8 +32,10 @@ def list():
         else:
             session['visits'] = 1 
             total_visits = session.get('visits')
-        return render_template("list.html",rows = rows, color = g.color, total_visits = total_visits)
-
+        try:
+            return render_template("list.html",rows = rows, color = g.color, total_visits = total_visits)
+        except Exception as e:
+            return (str(e))
     return redirect(url_for("index"))
 
 #show product details
@@ -40,10 +43,12 @@ def list():
 def list_product(count):
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
-    cur.execute("select * from products1 where count = " + count)
+    cur.execute("select * from products where count = " + count)
     row = cur.fetchone()
-    return render_template('list_id.html', row=row, color = g.color)
-
+    try:
+        return render_template('list_id.html', row=row, color = g.color)
+    except Exception as e:
+        return (str(e))
 
 #sessions
 @app.before_request
@@ -55,14 +60,16 @@ def before_request():
     if "color" in session:
         g.color = session["color"]
 
-@app.route('/getsession')
-def getsession():
-    if 'user' in session:
-        return session['user']
-
-    return 'Not logged in!'
-
 @app.route('/dropsession')
 def dropsession():
     session.pop('user', None)
+    session.pop('visits', None)
     return 'Dropped!'
+
+#Error handling
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("error.html")
+
+if __name__ == "__main__":
+    app.run()
--- wuyts_maxim.sql
+++ wuyts_maxim.sql
@@ -1,5 +1,6 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS `products1` (
+CREATE DATABASE IF NOT EXISTS `wuyts_maxim`;
+CREATE TABLE IF NOT EXISTS `products` (
 	`count`	INTEGER,
 	`brand`	TEXT,
 	`product_name`	TEXT,
@@ -7,11 +8,11 @@ CREATE TABLE IF NOT EXISTS `products1` (
 	`current_price`	INTEGER,
 	`total_ratings`	INTEGER
 );
-INSERT INTO `products1` VALUES (1,'Nubia','Nubia N1 Lite 4G LTE Dual SIM Unlocked Smartphone 5.5" 2GB RAM Black/Gold| US Warranty','131,99','70,99','9)');
-INSERT INTO `products1` VALUES (2,'SAMSUNG','Samsung Galaxy S7 Edge Dual SIM Unlocked Smart Phone| Dual Edge 5.5" AMOLED Display| Gold Color| 32GB Storage 4GB RAM International Version - No Warranty','693,99','503,9',46);
-INSERT INTO `products1` VALUES (3,'Apple','Apple iPhone 7 256GB Black Unlocked Smartphone','777,99','437,9','6)');
-INSERT INTO `products1` VALUES (4,'SAMSUNG','Samsung Galaxy S8+ (Plus) G955FD Dual SIM GSM Unlocked Smart Phone| 6.2" AMOLED Display| Maple Gold Color| 64GB Storage International Version - No Warranty','789,99','412,9',10);
-INSERT INTO `products1` VALUES (5,'Motorola','Moto G5 Plus XT1687 32GB Smartphone (Unlocked| Fine Gold) - US Warranty','202,99','176,9',66);
-INSERT INTO `products1` VALUES (6,'SAMSUNG','Samsung Galaxy Note 8 Dual SIM Unlocked Smartphone with LED Dual Camera (6.3" Black| 6GB RAM) International Version - No Warranty','751,99','503,9','1)');
-INSERT INTO `products1` VALUES (7,'Motorola','Moto G5s Plus (Special Edition) Unlocked Smartphone Dual Camera (5.5" Lunar Gray| 32GB Storage 3GB RAM) US Warranty','243,99','174,9',40);
+INSERT INTO `products` VALUES (1,'Nubia','Nubia N1 Lite 4G LTE Dual SIM Unlocked Smartphone 5.5" 2GB RAM Black/Gold| US Warranty','131,99','70,99','9)');
+INSERT INTO `products` VALUES (2,'SAMSUNG','Samsung Galaxy S7 Edge Dual SIM Unlocked Smart Phone| Dual Edge 5.5" AMOLED Display| Gold Color| 32GB Storage 4GB RAM International Version - No Warranty','693,99','503,9',46);
+INSERT INTO `products` VALUES (3,'SAMSUNG','Samsung Galaxy S8+ (Plus) G955FD Dual SIM GSM Unlocked Smart Phone| 6.2" AMOLED Display| Maple Gold Color| 64GB Storage International Version - No Warranty','789,99','412,9',10);
+INSERT INTO `products` VALUES (4,'Motorola','Moto G5 Plus XT1687 32GB Smartphone (Unlocked| Fine Gold) - US Warranty','202,99','176,9',66);
+INSERT INTO `products` VALUES (5,'SAMSUNG','Samsung Galaxy Note 8 Dual SIM Unlocked Smartphone with LED Dual Camera (6.3" Black| 6GB RAM) International Version - No Warranty','751,99','503,9','1)');
+INSERT INTO `products` VALUES (6,'Motorola','Moto G5s Plus (Special Edition) Unlocked Smartphone Dual Camera (5.5" Lunar Gray| 32GB Storage 3GB RAM) US Warranty','243,99','174,9',40);
+INSERT INTO `products` VALUES (7,'Apple','Apple iPhone 7 256GB Black Unlocked Smartphone','777,99','437,9','6)');
 COMMIT;
